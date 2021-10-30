4b985e1a664458fcda5ea5f0d5c0ebea547d613d
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-10-30 14:14:23 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Index from "../src/component/Index";
+
+const IndexPage: NextPage = () => {
+  return <Index />;
+};
+
+export default IndexPage;
---
+++ src/component/Index.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+
+const Index: React.FC = () => {
+  return <Box p="2">Hello</Box>;
+};
+
+export default Index;
