426e30777e0781b9de32cb13f873e4e523ec43fb
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-02 18:31:56 +0530

    Added images

--- src/component/project/ProjectsList.tsx
+++ src/component/project/ProjectsList.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, HStack, Heading, Flex } from "@chakra-ui/layout";
+import { Box, Text, Heading, Flex } from "@chakra-ui/layout";
 
 const projects = [
   {
@@ -13,7 +13,7 @@ const projects = [
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna.`,
     percentageFunded: 25,
-    imageUrl: "lost_torch.png",
+    imageUrl: "blood_shaman.png",
   },
 ];
 
@@ -39,8 +39,11 @@ function Project(props: any) {
   return (
     <Box mt="5" border="2px solid black">
       <Flex minHeight="250px" flexDirection="row" p="5">
-        <Box width="50%" color="black"></Box>
-        <Box position="relative" width="50%" color="black">
+        <Box pr="5" width="50%" color="black">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img width="100%" src={`images/${imageUrl}`} alt={title}></img>
+        </Box>
+        <Box pl="5" position="relative" width="50%" color="black">
           <Text>{`${percentageFunded}% funded`}</Text>
           <Heading mt="2" size="lg">
             {title}
