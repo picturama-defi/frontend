6b3fa6bc23fa89a619534d677b0d4cfc4101f354
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-03 11:45:10 +0530

    video added

--- src/component/project-detail/index.tsx
+++ src/component/project-detail/index.tsx
@@ -1,7 +1,14 @@
 import Header from "./Header";
+import VideoContainer from "../common/VideoContainer";
+import { highlightedProject } from "../../hardCodedData";
 
 function ProjectDetail() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <VideoContainer showDescription={false} details={highlightedProject} />
+    </>
+  );
 }
 
 export default ProjectDetail;
