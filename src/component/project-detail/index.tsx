80b1c928dc133ee43e852ec3d5a85d0aa8e2d7b7
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-03 13:53:39 +0530

    added ticker

--- src/component/project-detail/index.tsx
+++ src/component/project-detail/index.tsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import VideoContainer from "../common/VideoContainer";
 import { highlightedProject } from "../../hardCodedData";
+import Ticker from "./Ticker";
 
 function ProjectDetail() {
   return (
     <>
       <Header />
       <VideoContainer showDescription={false} details={highlightedProject} />
+      <Ticker />
     </>
   );
 }
