8d80905353f8240190fee786537d2d62066c2f79
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-24 09:06:21 +0530

    staking forim

--- src/component/project-detail/StakingDetails.tsx
+++ src/component/project-detail/StakingDetails.tsx
@@ -71,7 +71,7 @@ const Row3 = () => {
     <HStack pt="5" pb="5" borderTop="1px solid black">
       <Box w="30%">
         <Box>TVL</Box>
-        <Text fontWeight="bold">$ALGO</Text>
+        <Text fontWeight="bold">$MATIC</Text>
         <Text fontWeight="bold">111,979,625</Text>
         <Box>$1,740.163</Box>
       </Box>
