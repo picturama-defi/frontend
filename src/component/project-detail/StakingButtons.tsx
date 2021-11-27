1aa6349882a81ffc3aca174c4a2a49cf51ff412e
Sanjay <sanjayrjs16@gmail.com>
2021-11-28 00:33:52 +0530

    Modals added

--- src/component/project-detail/StakingButtons.tsx
+++ src/component/project-detail/StakingButtons.tsx
@@ -1,12 +1,48 @@
 import { Button, Flex } from "@chakra-ui/react";
-import { fundProject, claim } from "../../API/contract/main";
+import { fundProject, claim, withdraw } from "../../API/contract/main";
+import React, { useState } from "react";
+import BasicModal from "../common/BasicModal";
 
-function StakingButtons({ id, setLoading, fetchFilm }: any) {
-  const handleClick = async () => {
+function StakingButtons({
+  id,
+  setLoading,
+  fetchFilm,
+  showWithdrawClaimBtn,
+}: any) {
+  const [showWithdrawModal, setShowWithdrawModal] = useState(false);
+  const [showStakeModal, setShowStakeModal] = useState(false);
+  const [stakeAmount, setStakeAmount] = useState(0);
+
+  const onConfirmWithdraw = async () => {
     setLoading(true);
-    await fundProject(id);
+    await withdraw(id);
     setLoading(false);
     fetchFilm();
+  };
+
+  const closeWithDrawModal = () => {
+    setShowWithdrawModal(false);
+  };
+
+  const handleWithdrawClick = async () => {
+    setShowWithdrawModal(true);
+  };
+
+  const onConfirmStake = async () => {
+    setLoading(true);
+    const res = await fundProject(id, stakeAmount);
+    if (!res) alert("Transaction failed!");
+    setLoading(false);
+    fetchFilm();
+  };
+
+  const closeStakeModal = () => {
+    setStakeAmount(0);
+    setShowStakeModal(false);
+  };
+
+  const handleStakeClick = async () => {
+    setShowStakeModal(true);
   };
 
   const handleClaimClick = async () => {
@@ -17,17 +53,57 @@ function StakingButtons({ id, setLoading, fetchFilm }: any) {
   };
 
   return (
-    <Flex justifyContent="space-around" p="5" flexDirection="row">
-      <Button onClick={handleClick} width="25%" variant="brand2">
-        STAKE
-      </Button>
-      <Button width="25%" ml="2" variant="brand2">
-        WITHDRAW
-      </Button>
-      <Button onClick={handleClaimClick} width="25%" ml="2" variant="brand2">
-        CLAIM
-      </Button>
-    </Flex>
+    <>
+      <Flex justifyContent="space-around" p="5" flexDirection="row">
+        <Button onClick={handleStakeClick} width="25%" variant="brand2">
+          STAKE
+        </Button>
+        {showWithdrawClaimBtn && (
+          <>
+            <Button
+              onClick={handleWithdrawClick}
+              width="25%"
+              ml="2"
+              variant="brand2"
+            >
+              WITHDRAW
+            </Button>
+            <Button
+              onClick={handleClaimClick}
+              width="25%"
+              ml="2"
+              variant="brand2"
+            >
+              CLAIM
+            </Button>
+          </>
+        )}
+
+        {showWithdrawModal && (
+          <BasicModal
+            onOpen={showWithdrawModal}
+            onClose={closeWithDrawModal}
+            onConfirm={onConfirmWithdraw}
+            title={"Are you sure to withdraw your deposit?"}
+            description={
+              "Withdrawing will return your invested ETH, but you will lose the RAMA rewards accumilated"
+            }
+          />
+        )}
+        {showStakeModal && (
+          <BasicModal
+            onOpen={showStakeModal}
+            onClose={closeStakeModal}
+            onConfirm={onConfirmStake}
+            title={"Fund this film"}
+            description={"Enter the amount to stake "}
+            withInput={true}
+            stakeAmount={stakeAmount}
+            setStakeAmount={setStakeAmount}
+          />
+        )}
+      </Flex>
+    </>
   );
 }
 
