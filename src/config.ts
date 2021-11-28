9615b36ecb2ec2a6893626712bf9864b549adbb1
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-28 11:36:11 +0530

    changed config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://picturama-backend.herokuapp.com/";
 export const ADMIN_PUBLIC_ADDRESS =
-  "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
-export const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-export const CORRECT_CHAIN = "0x539";
-export const RAMA_TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0xB6ccdEcd1a433a39911D6C5cBfEbC29Ee6FE85FF";
+export const CONTRACT_ADDRESS = "0x9b751c1d4Eb659850A41d52bf3397077BE0561be";
+export const CORRECT_CHAIN = "4";
+export const RAMA_TOKEN_ADDRESS = "0x971803fB6FABa2B6eAF6e7ee08e8b26BEbA728B9";
