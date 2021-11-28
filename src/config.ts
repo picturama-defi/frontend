65fa74d05f7b9eebe7d1bd67c8932f8f59afd94a
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-28 11:51:46 +0530

    address correction

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://picturama-backend.herokuapp.com/";
+export const BASE_URL = "https://picturama-backend.herokuapp.com";
 export const ADMIN_PUBLIC_ADDRESS =
   "0xB6ccdEcd1a433a39911D6C5cBfEbC29Ee6FE85FF";
 export const CONTRACT_ADDRESS = "0x9b751c1d4Eb659850A41d52bf3397077BE0561be";
